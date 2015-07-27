e46cf76857983e745084899ffe9c4945575d026d
Kevin <kpm82@cornell.edu>
2015-07-27 12:03:36 +0200

    hacky way to add branch to make data and MC weights match

---
+++ hack_wgtsum.C
@@ -0,0 +1,22 @@
+void hack_wgtsum(TString dir) {
+  TFile* infile = new TFile(Form("Data/%s/tree.root",dir.Data()));
+  TTree* frtree = (TTree*)infile->Get("tree/tree");
+  if (!frtree) return;
+
+  double wgtsum = 1.0;
+  TBranch* bwgtsum = frtree->Branch("wgtsum", &wgtsum, "wgtsum/D");
+
+  for (Long64_t i = 0; i < frtree->GetEntries(); i++){
+    bwgtsum->Fill();
+  }
+
+  TFile* outfile = new TFile(Form("Data/%s/treewithwgt.root",dir.Data()),"RECREATE");
+  outfile->cd();
+  TDirectoryFile* treedir = new TDirectoryFile("tree", "tree");
+  treedir->cd();
+  TTree* outtree = frtree->CopyTree("");
+  outfile->Write();
+
+  infile->Close();
+  outfile->Close();
+}
