9116e271785611d57076647f8e80d98051cea91b
Kevin <kpm82@cornell.edu>
2015-07-27 12:02:57 +0200

    add file for setting cpp11

---
+++ MacroCommon.hh
@@ -0,0 +1,11 @@
+void setupcpp11(){ // customize ACLiC's behavior ...
+  TString o;
+  // customize MakeSharedLib
+  o = TString(gSystem->GetMakeSharedLib());
+  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
+  gSystem->SetMakeSharedLib(o.Data());
+  // customize MakeExe
+  o = TString(gSystem->GetMakeExe());
+  o = o.ReplaceAll(" -c ", " -std=c++0x -c ");
+  gSystem->SetMakeExe(o.Data());
+} 
