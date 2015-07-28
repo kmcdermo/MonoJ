6dbc87b9e7f110bcac76e6952fd8c081e560ee57
Kevin McDermott <kpm82@cornell.edu>
2015-07-28 11:52:46 +0200

    add these to common framework

---
+++ Typedef.cc
@@ -0,0 +1,40 @@
+#include "Typedef.hh"
+
+void MakeOutDirectory(TString outdir){
+  FileStat_t dummyFileStat;
+  if (gSystem->GetPathInfo(outdir.Data(), dummyFileStat) == 1){
+    TString mkDir = "mkdir -p ";
+    mkDir += outdir.Data();
+    gSystem->Exec(mkDir.Data());
+  }
+}
+
+void CheckValidFile(TFile *& file, const TString fname){
+  if (file == (TFile*) NULL) { // check if valid file
+    std::cout << "Input file is bad pointer: " << fname.Data() << " ...exiting..." << std::endl;
+    exit(1);
+  }
+  else {
+    std::cout << "Successfully opened file: " << fname.Data() << std::endl;
+  }
+}
+
+void CheckValidTree(TTree*& tree, const TString tname, const TString fname){
+  if (tree == (TTree*) NULL) { // check if valid plot
+    std::cout << "Input TTree is bad pointer: " << tname.Data() << " in input file: " << fname.Data() << " ...exiting..." << std::endl;
+    exit(1);
+  }
+  else {
+    //    std::cout << "Successfully opened tree: " << tname.Data() << " in input file: " << fname.Data() << std::endl;
+  }
+}
+
+void CheckValidTH1D(TH1D*& plot, const TString pname, const TString fname){
+  if (plot == (TH1D*) NULL) { // check if valid plot
+    std::cout << "Input TH1D is bad pointer: " << pname.Data() << " in input file: " << fname.Data() << " ...exiting..." << std::endl;
+    exit(1);
+  }
+  else {
+    //    std::cout << "Successfully initialized plot: " << pname.Data() << " in input file: " << fname.Data() << std::endl;
+  }
+}
---
+++ Typedef.hh
@@ -0,0 +1,31 @@
+#ifndef _typedef_
+#define _typedef_
+
+#include "TString.h"
+#include "TColor.h"
+#include "TSystem.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "TTree.h"
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+
+typedef std::map<TString,Color_t> ColorMap;
+typedef std::vector<TString>      TStrVec;
+
+typedef std::pair<TString,Bool_t> SamplePair;
+typedef std::vector<SamplePair>   SamplePairVec;
+typedef SamplePairVec::iterator   SamplePairVecIter;
+
+// global variables
+// global functions
+
+void MakeOutDirectory(TString outdir);
+void CheckValidFile(TFile *& file, const TString fname);
+void CheckValidTree(TTree*& tree, const TString tname, const TString fname);
+void CheckValidTH1D(TH1D*& plot, const TString pname, const TString fname);
+
+#endif
