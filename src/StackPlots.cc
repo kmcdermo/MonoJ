a9b2dd25c751ca6dcdc9dfb115bc543ffffd85fc
Kevin <kpm82@cornell.edu>
2015-07-31 00:54:48 +0200

    made Lumi correct, will need to change as it goes up

--- src/StackPlots.cc
+++ src/StackPlots.cc
@@ -362,7 +362,7 @@ void StackPlots::CMSLumi(TCanvas *& canv, const Int_t iPosX) { // borrowed from 
   TString extraText      = "Preliminary";
   Double_t extraTextFont = 52;  // default is helvetica-italics
 
-  TString lumiText = Form("#sqrt{s} = 13 TeV, L = %f fb^{-1}", fLumi);
+  TString lumiText = Form("#sqrt{s} = 13 TeV, L = %1.5f fb^{-1}", fLumi); // must change this spec once we are in fb range!
   
   // text sizes and text offsets with respect to the top frame
   // in unit of the top margin size
