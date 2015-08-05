9a6f7851efb2adc6cdeb8a36ea4b140e68757e83
Kevin <kpm82@cornell.edu>
2015-08-06 00:30:53 +0200

    make copy plots not delete stuff again

--- scripts/copyplots.sh
+++ scripts/copyplots.sh
@@ -13,14 +13,29 @@ move=$3
 #cd -
 
 if [ ! -d ~/www/${dir} ] ; then
-    mkdir -p ~/www${dir}
+    echo "making directory ~/www/${dir}"
+    mkdir -p ~/www/${dir}
 fi
 
+if [ ! -d ~/www/${subdir} ] ; then
+    echo "making subdirectory ~/www/${subdir}"
+    mkdir -p ~/www/${subdir}
+#else
+#    echo "removing then making directory ~/www/${subdir}"
+#    rm -rf ~/www/${subdir}
+#    mkdir -p ~/www/${subdir}
+fi
 
 if [ "${move}" == true ] ; then
+    echo "Moving PURW plots"
+    mv ${dir}/nvtx_beforePURW* ~/www/${dir}
+    mv ${dir}/nvtx_afterPURW* ~/www/${dir}
     echo "Moving directory: ${subdir} to ~/www/{dir}" 
-    mv ${dir} ~/www/
+    mv ${subdir} ~/www/${subdir}
 else
+    echo "Copying PURW plots"
+    cp ${dir}/nvtx_beforePURW* ~/www/${dir}
+    cp ${dir}/nvtx_afterPURW* ~/www/${dir}
     echo "Copying in new directory: ${subdir} to ~/www/${dir}" 
     cp -r ${subdir} ~/www/${dir}
 fi
